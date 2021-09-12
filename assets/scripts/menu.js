d23f20052b553acb661c26189aa20e281856e404
Ryuka <hiukakamachi@gmail.com>
2021-09-11 23:10:39 -0300

    v5.0

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -26,37 +26,6 @@ function changeHeaderWhenScroll() {
   }
 }
 
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
-})
-
-const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
-  reset: true
-})
-
-scrollReveal.reveal(
-  `#home .text, #home .image,
-  #about .image, #about .text,
-  #services header, #services .card,
-  #testimonials header, #testimonials .testimonials,
-  #contact .links, footer .brand, footer .social
-  `,
-  { interval: 100 }
-)
-
 const backToTopButton = document.querySelector('.back-to-top')
 
 function backToTop() {
@@ -77,7 +46,7 @@ function activateMenuAtCurrentSection() {
     const sectionId = section.getAttribute('id')
 
     const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = chechpoint <= sectionTop + sectionHeight
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
 
     if (checkpointStart && checkpointEnd) {
       document
